8d05dad3700395898368c9fdb08dbf8498114628
Felix Hiller <felix.a.hiller@gmail.com>
2016-10-14 15:38:46 +0100

    Updated readme

--- bisection.ml
+++ bisection.ml
@@ -1,13 +1,13 @@
 iterative (f,a,b,TOL,NMAX) {
 	n := 1;
 	while ( n <= NMAX ) do  {
-		c := ((a + b) / 2);
-		if f(c) = 0 | (b-a)/2 > TOL do {
-			Output(c);
+		c := ((!a + !b) / 2);
+		if f(!c) = 0 | (!b-!a)/2 > TOL do {
+			Output(!c);
 			Stop;
 		};
 		n := n + 1;
-		if sight( f(c) ) = sight( f(a) ) do a := c else b := c;
+		if sight( f(!c) ) = sight( f(!a) ) do a := !c else b := !c;
 	};
 	Output("Method failed.");
 };
@@ -15,9 +15,9 @@ iterative (f,a,b,TOL,NMAX) {
 recursive (f,a,b,TOL,NMAX,n) {
 	n := 1;
 	if n < NMAX do {
-		c := ((a + b) / 2);
-		if f(c) = 0 | (b-a)/2 > TOL do {
-			return(c);
+		c := ((!a + !b) / 2);
+		if f(!c) = 0 | (!b-!a)/2 > TOL do {
+			return(!c);
 		};
 		recursive(f,a,b,TOL,NMAX,(n+1))
 	}
